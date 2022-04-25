30918e31a9c7aae6ea275bfbac8f784bbd9e7268
cw66786 <cw66786@gmail.com>
2022-04-24 23:59:02 -0500

    redirect page added for after form submit

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
 
-if (isset($_POST['submit'])) {
+    $_POST['submit'];
 
     $name =$_POST['name'];
 
@@ -17,10 +17,61 @@ $header = "From: ".$emailFrom;
 $txt = "Portfolio email from ".$name.".\n\n".$message;
 
 
-mail($mailto,  $subject, $txt, $header);
+mail($mailto,  $subject, $txt, $header) or die("Oh no, something went wrong!");
 
-header("Location: index.php?mailsend");
 
+echo '
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            *{
+                padding: 0;
+                margin: 0;
+                
+            }
+
+            .container {
+                padding:0;
+                margin:0;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: flex-start;
+                background-color: #1c1c1c;
+                height: 100vh;
+                width: 100vw;
+
+            }
+            .thanks {
+                color: #9d9d9d;
+            font-family: "Merienda", cursive;
+            font-size: 80px;
+            }
+
+
+            .thanks p {
+                color: #9d9d9d;
+            font-family: "Merienda", cursive;
+            font-size: 40px;
+            }
+
+            .thanks a {
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+    <div id="contaner" class="container">
+    <h1 id="thanks" class="thanks">Thank you for your time.</h1>
+    <h1 id="thanks" class="thanks>I'll get back to you as soon as possible.</h1>
+    <p>Return to<a href="index.html">Portfolio</a>.</p>
+</div>
+</body>
+    </html>
+    
+    '
 
 }
 
